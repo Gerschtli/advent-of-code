fac4bb6ae1fa720861e666fd378db3499cfebbed
Tobias Happ <tobias.happ@gmx.de>
2020-12-01 17:58:09 +0100

    2020/01: complete implementation for part 2

--- 2020/01/main.go
+++ 2020/01/main.go
@@ -15,12 +15,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	number1, number2, err := findMatchingNumbers(numbers)
+	number1, number2, err := findMatchingPair(numbers)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("found numbers (%v, %v), product is %v\n", number1, number2, number1*number2)
+
+	number1, number2, number3, err := findMatchingTriple(numbers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("found numbers (%v, %v, %v), product is %v\n", number1, number2, number3, number1*number2*number3)
 }
 
 func loadNumbers(filename string) ([]int, error) {
@@ -52,7 +59,7 @@ func loadNumbers(filename string) ([]int, error) {
 	return numbers, nil
 }
 
-func findMatchingNumbers(numbers []int) (int, int, error) {
+func findMatchingPair(numbers []int) (int, int, error) {
 	numbersCount := len(numbers)
 
 	for i := 0; i < numbersCount-1; i++ {
@@ -63,5 +70,21 @@ func findMatchingNumbers(numbers []int) (int, int, error) {
 		}
 	}
 
-	return 0, 0, errors.New("no matching numbers found")
+	return 0, 0, errors.New("no matching numbers pair found")
+}
+
+func findMatchingTriple(numbers []int) (int, int, int, error) {
+	numbersCount := len(numbers)
+
+	for i := 0; i < numbersCount-2; i++ {
+		for j := i + 1; j < numbersCount-1; j++ {
+			for k := j + 1; k < numbersCount; k++ {
+				if numbers[i]+numbers[j]+numbers[k] == 2020 {
+					return numbers[i], numbers[j], numbers[k], nil
+				}
+			}
+		}
+	}
+
+	return 0, 0, 0, errors.New("no matching numbers triple found")
 }
