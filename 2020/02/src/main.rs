37ac999b67ac674d22b7ecfb3cbb4304807da724
Tobias Happ <tobias.happ@gmx.de>
2020-12-03 20:47:40 +0100

    2020/02: add read_lines function

--- 2020/02/src/main.rs
+++ 2020/02/src/main.rs
@@ -1,17 +1,49 @@
-fn main() {
-    println!("{}", text());
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::path;
+
+fn main() -> Result<(), io::Error> {
+    let lines = read_lines("./files/passwords.txt")?;
+
+    println!("Result: {:#?}", lines);
+
+    Ok(())
 }
 
-fn text() -> &'static str {
-    "Hello, world!"
+fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<path::Path>,
+{
+    let file = fs::File::open(filename)?;
+    let lines_raw = io::BufReader::new(file).lines();
+
+    lines_raw.into_iter().collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hamcrest2::prelude::*;
 
     #[test]
-    fn text_test() {
-        assert_eq!(text(), "Hello, world!");
+    fn read_lines_returns_every_line_from_file() {
+        let lines = read_lines("./files/example.txt");
+
+        assert_that!(
+            lines,
+            has(vec![
+                "abc".to_string(),
+                "bcd".to_string(),
+                "cde".to_string()
+            ])
+        );
+    }
+
+    #[test]
+    fn read_lines_returns_error_when_file_not_found() {
+        let lines = read_lines("./files/example_not_found.txt");
+
+        assert_that!(lines, err());
     }
 }
