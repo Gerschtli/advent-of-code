746b92a64a8a41ed34fd489512a7cd734e94e732
Tobias Happ <tobias.happ@gmx.de>
2020-12-04 16:44:53 +0100

    2020/03: implement part 1

---
+++ 2020/03/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	m, err := loadMap("./files/map.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	position := Position{}
+	count := 0
+	for {
+		isBlocked, bottomReached := m.get(&position)
+		if bottomReached {
+			break
+		}
+
+		if isBlocked {
+			count++
+		}
+
+		position = position.move()
+	}
+
+	log.Printf("%v trees found", count)
+}
+
+func loadMap(filename string) (Map, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("error occured on file close: %v", err)
+		}
+	}()
+
+	var m Map
+	scanner := bufio.NewScanner(file)
+	var length int
+	for i := 0; scanner.Scan(); i++ {
+		m = append(m, []bool{})
+
+		for _, char := range scanner.Text() {
+			var isBlocked bool
+
+			switch char {
+			case '.':
+				isBlocked = false
+			case '#':
+				isBlocked = true
+			default:
+				return nil, errors.New(fmt.Sprintf("unknown char found: %q", char))
+			}
+
+			m[i] = append(m[i], isBlocked)
+		}
+
+		if i == 0 {
+			length = len(m[i])
+		} else if len(m[i]) != length {
+			return nil, errors.New("provided map is not a rectangle")
+
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
---
+++ 2020/03/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestMainPrintsTreeCount(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	main()
+	log.SetOutput(os.Stdout)
+
+	assert.Contains(t, buf.String(), "189 trees found")
+}
+
+func TestLoadMapReturnsMap(t *testing.T) {
+	m, err := loadMap("./files/map_example.txt")
+
+	assert.Nil(t, err)
+	assert.Equal(t, Map{
+		{false, false, true, true, false, false},
+		{true, false, false, false, true, false},
+		{false, true, false, false, false, false},
+		{false, false, true, false, true, false},
+	}, m)
+}
+
+func TestLoadMapReturnsErrorWhenFileNotFound(t *testing.T) {
+	_, err := loadMap("./files/map_not_found")
+
+	assert.IsType(t, &os.PathError{}, err)
+}
+
+func TestLoadMapReturnsErrorWhenUnknownCharInMap(t *testing.T) {
+	_, err := loadMap("./files/map_unknown_char.txt")
+
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "unknown char found: '!'", err.Error())
+	}
+}
+
+func TestLoadMapReturnsErrorWhenLinesNotEquallySized(t *testing.T) {
+	_, err := loadMap("./files/map_line_length_mismatch.txt")
+
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "provided map is not a rectangle", err.Error())
+	}
+}
