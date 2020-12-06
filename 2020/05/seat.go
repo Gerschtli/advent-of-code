2bb83e9d9a2a0719cc4de9d6d33df88bcd132dbf
Tobias Happ <tobias.happ@gmx.de>
2020-12-06 13:49:25 +0100

    2020/05: move seat to separate file

---
+++ 2020/05/seat.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type seat struct {
+	row    int
+	column int
+}
+
+func (s *seat) id() int {
+	return s.row*8 + s.column
+}
+
+func buildSeatByCode(code string) (seat, error) {
+	if len(code) != 10 {
+		return seat{}, errors.New(fmt.Sprintf("code invalid: need 10 chars [%v]", code))
+	}
+
+	row, err := getCodeValue(code, code[:7], 127, 'F', 'B')
+	if err != nil {
+		return seat{}, err
+	}
+
+	column, err := getCodeValue(code, code[7:], 7, 'L', 'R')
+	if err != nil {
+		return seat{}, err
+	}
+
+	return seat{row, column}, nil
+}
+
+func getCodeValue(code string, codePart string, high int, charLow int32, charHigh int32) (int, error) {
+	var low, step int
+	for _, char := range codePart {
+		step = (high - low + 1) / 2
+		switch char {
+		case charHigh:
+			low += step
+		case charLow:
+			high -= step
+		default:
+			return 0, errors.New(fmt.Sprintf("code invalid: invalid char, need %c or %c [%v]", charLow, charHigh, code))
+		}
+	}
+
+	return low, nil
+}
---
+++ 2020/05/seat_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSeatIdReturnsId(t *testing.T) {
+	s := seat{70, 7}
+	assert.Equal(t, 567, s.id())
+
+	s = seat{14, 7}
+	assert.Equal(t, 119, s.id())
+
+	s = seat{102, 4}
+	assert.Equal(t, 820, s.id())
+}
+
+func TestBuildSeatByCodeReturnsValidSeats(t *testing.T) {
+	s, err := buildSeatByCode("BFFFBBFRRR")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{70, 7}, s)
+
+	s, err = buildSeatByCode("FFFBBBFRRR")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{14, 7}, s)
+
+	s, err = buildSeatByCode("BBFFBBFRLL")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{102, 4}, s)
+}
+
+func TestBuildSeatByCodeReturnsErrorsForInvalidCodes(t *testing.T) {
+	_, err := buildSeatByCode("BFFFBBFRR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: need 10 chars [BFFFBBFRR]", err.Error())
+	}
+
+	_, err = buildSeatByCode("FFFBBBRRRR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: invalid char, need F or B [FFFBBBRRRR]", err.Error())
+	}
+
+	_, err = buildSeatByCode("BFFFBBFLAR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: invalid char, need L or R [BFFFBBFLAR]", err.Error())
+	}
+}
