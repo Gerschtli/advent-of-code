b74188f4f2352d590e91fc850eab44f44851b205
Tobias Happ <tobias.happ@gmx.de>
2020-12-06 15:04:35 +0100

    2020/05: add offset to plane type

--- 2020/05/plane.go
+++ 2020/05/plane.go
@@ -1,28 +1,31 @@
 package main
 
-type plane [][]bool
+type plane struct {
+	seats  [][]bool
+	offset int
+}
 
 func buildPlane(maxRows int, maxColumns int) plane {
-	p := make([][]bool, maxRows, maxRows)
+	seats := make([][]bool, maxRows, maxRows)
 
-	for r := range p {
-		p[r] = make([]bool, maxColumns, maxColumns)
+	for r := range seats {
+		seats[r] = make([]bool, maxColumns, maxColumns)
 	}
 
-	return p
+	return plane{seats, 0}
 }
 
 func (p *plane) markSeatTaken(s *seat) {
-	(*p)[s.row][s.column] = true
+	p.seats[s.row-p.offset][s.column] = true
 }
 
 func (p *plane) getFreeSeats() []seat {
 	var seats []seat
 
-	for r, row := range *p {
+	for r, row := range p.seats {
 		for c, taken := range row {
 			if !taken {
-				seats = append(seats, seat{r, c})
+				seats = append(seats, seat{r + p.offset, c})
 			}
 		}
 	}
--- 2020/05/plane_test.go
+++ 2020/05/plane_test.go
@@ -9,22 +9,31 @@ import (
 func TestBuildPlaneInitialisesWithFalse(t *testing.T) {
 	p := buildPlane(2, 2)
 
-	assert.Equal(t, plane([][]bool{{false, false}, {false, false}}), p)
+	assert.Equal(t, plane{seats: [][]bool{{false, false}, {false, false}}}, p)
 }
 
 func TestPlaneMarkSeatTaken(t *testing.T) {
 	s := seat{1, 0}
-	p := plane([][]bool{{false, false}, {false, false}})
+	p := plane{seats: [][]bool{{false, false}, {false, false}}}
 
 	p.markSeatTaken(&s)
 
-	assert.Equal(t, true, p[1][0])
+	assert.Equal(t, true, p.seats[1][0])
+}
+
+func TestPlaneMarkSeatTakenWithOffset(t *testing.T) {
+	s := seat{1, 0}
+	p := plane{seats: [][]bool{{false, false}, {false, false}}, offset: 1}
+
+	p.markSeatTaken(&s)
+
+	assert.Equal(t, true, p.seats[0][0])
 }
 
 func TestPlaneGetFreeSeatsReturnsSliceOfSeats(t *testing.T) {
-	p := plane([][]bool{{false, true}, {true, false}})
+	p := plane{seats: [][]bool{{false, true}, {true, false}}, offset: 1}
 
 	seats := p.getFreeSeats()
 
-	assert.Equal(t, []seat{{0, 0}, {1, 1}}, seats)
+	assert.Equal(t, []seat{{1, 0}, {2, 1}}, seats)
 }
