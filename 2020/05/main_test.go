251bf1f7f5f94defb7ae62d0f18e612c94e8919d
Tobias Happ <tobias.happ@gmx.de>
2020-12-06 13:44:23 +0100

    2020/05: add buildSeatByCode

--- 2020/05/main_test.go
+++ 2020/05/main_test.go
@@ -21,3 +21,34 @@ func TestMainLogsResults(t *testing.T) {
 	assert.Len(t, lines, 1)
 	assert.Empty(t, lines[0])
 }
+
+func TestBuildSeatByCodeReturnsValidSeats(t *testing.T) {
+	s, err := buildSeatByCode("BFFFBBFRRR")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{70, 7}, s)
+
+	s, err = buildSeatByCode("FFFBBBFRRR")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{14, 7}, s)
+
+	s, err = buildSeatByCode("BBFFBBFRLL")
+	assert.Nil(t, err)
+	assert.Equal(t, seat{102, 4}, s)
+}
+
+func TestBuildSeatByCodeReturnsErrorsForInvalidCodes(t *testing.T) {
+	_, err := buildSeatByCode("BFFFBBFRR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: need 10 chars [BFFFBBFRR]", err.Error())
+	}
+
+	_, err = buildSeatByCode("FFFBBBRRRR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: invalid char, need F or B [FFFBBBRRRR]", err.Error())
+	}
+
+	_, err = buildSeatByCode("BFFFBBFLAR")
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: invalid char, need L or R [BFFFBBFLAR]", err.Error())
+	}
+}
