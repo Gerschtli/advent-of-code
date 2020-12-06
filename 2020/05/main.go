b3a5ef0f94ffd80b9ede8303c9b98b5c0b14e596
Tobias Happ <tobias.happ@gmx.de>
2020-12-06 13:56:40 +0100

    2020/05: add readSeats function

--- 2020/05/main.go
+++ 2020/05/main.go
@@ -1,4 +1,26 @@
 package main
 
+import "github.com/Gerschtli/advent-of-code/lib/go/file"
+
 func main() {
+}
+
+func readSeats(filename string) ([]seat, error) {
+	var seats []seat
+
+	err := file.ReadFile(filename, func(index int, line string) error {
+		s, err := buildSeatByCode(line)
+		if err != nil {
+			return err
+		}
+
+		seats = append(seats, s)
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return seats, nil
 }
--- 2020/05/main_test.go
+++ 2020/05/main_test.go
@@ -21,3 +21,22 @@ func TestMainLogsResults(t *testing.T) {
 	assert.Len(t, lines, 1)
 	assert.Empty(t, lines[0])
 }
+
+func TestReadSeatsReturnsSliceOfSeats(t *testing.T) {
+	seats, err := readSeats("./files/example.txt")
+
+	assert.Nil(t, err)
+	assert.Equal(t, []seat{
+		{70, 7},
+		{14, 7},
+		{102, 4},
+	}, seats)
+}
+
+func TestReadSeatsReturnsErrorForInvalidInput(t *testing.T) {
+	_, err := readSeats("./files/example_invalid.txt")
+
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "code invalid: need 10 chars [FFFBBBFRR]", err.Error())
+	}
+}
