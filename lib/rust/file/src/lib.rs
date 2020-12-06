3bdb49561b5e3de04a011fd867fd392b43ed5ec5
Tobias Happ <tobias.happ@gmx.de>
2020-12-06 12:17:54 +0100

    lib/rust/file: add file lib for rust

---
+++ lib/rust/file/src/lib.rs
@@ -0,0 +1,46 @@
+// necessary for intellij support
+#[cfg(test)]
+#[macro_use]
+extern crate hamcrest2;
+
+use std::io::BufRead;
+use std::{fs, io, path};
+
+pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<path::Path>,
+{
+    let file = fs::File::open(filename)?;
+    let lines_raw = io::BufReader::new(file).lines();
+
+    lines_raw.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use hamcrest2::prelude::*;
+
+    use super::*;
+
+    #[test]
+    fn read_lines_returns_every_line_from_file() {
+        let lines = read_lines("./files/example.txt");
+
+        assert_that!(
+            lines,
+            has(vec![
+                "abc".to_string(),
+                "bcd".to_string(),
+                "".to_string(),
+                "cde".to_string(),
+            ])
+        );
+    }
+
+    #[test]
+    fn read_lines_returns_error_when_file_not_found() {
+        let lines = read_lines("./files/example_not_found.txt");
+
+        assert_that!(lines, err());
+    }
+}
