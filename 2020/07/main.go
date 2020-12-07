687ebbacfff960d2fd3c8b6d009e58d01bc29231
Tobias Happ <tobias.happ@gmx.de>
2020-12-07 23:17:54 +0100

    2020/07: complete part 1

--- 2020/07/main.go
+++ 2020/07/main.go
@@ -1,4 +1,81 @@
 package main
 
+import (
+	"log"
+	"regexp"
+
+	"github.com/Gerschtli/advent-of-code/lib/go/file"
+)
+
+var rulesRegex = regexp.MustCompile(`(\w+ \w+) bags?`)
+
+type rules map[color][]color
+type containable map[color][]color
+type color string
+
 func main() {
+	ruleSet, err := readRules("./files/rules.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	containableMap := transposeRules(&ruleSet)
+	bagColorsForShinyGold := getTransitiveContainableForColor(&containableMap, "shiny gold")
+
+	log.Printf("%d bag colors can contain shiny gold", len(bagColorsForShinyGold))
+}
+
+func readRules(filename string) (rules, error) {
+	ruleSet := rules(map[color][]color{})
+
+	err := file.ReadFile(filename, func(index int, line string) error {
+		matches := rulesRegex.FindAllStringSubmatch(line, -1)
+		key := color(matches[0][1])
+		ruleSet[key] = []color{}
+
+		for _, match := range matches[1:] {
+			if match[1] == "no other" {
+				continue
+			}
+
+			ruleSet[key] = append(ruleSet[key], color(match[1]))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ruleSet, nil
+}
+
+func transposeRules(ruleSet *rules) containable {
+	containableMap := containable(map[color][]color{})
+
+	for key, values := range *ruleSet {
+		for _, value := range values {
+			containableMap[value] = append(containableMap[value], key)
+		}
+	}
+
+	return containableMap
+}
+
+func getTransitiveContainableForColor(containableMap *containable, c color) map[color]bool {
+	values, ok := (*containableMap)[c]
+	if !ok {
+		return nil
+	}
+
+	colors := map[color]bool{}
+	for _, v := range values {
+		colors[v] = true
+		for key := range getTransitiveContainableForColor(containableMap, v) {
+			colors[key] = true
+		}
+	}
+
+	return colors
 }
--- 2020/07/main_test.go
+++ 2020/07/main_test.go
@@ -18,6 +18,80 @@ func TestMainLogsResults(t *testing.T) {
 
 	lines := strings.Split(buf.String(), "\n")
 
-	assert.Len(t, lines, 1)
-	assert.Empty(t, lines[0])
+	assert.Len(t, lines, 2)
+	assert.Contains(t, lines[0], "131 bag colors can contain shiny gold")
+	assert.Empty(t, lines[1])
+}
+
+func TestReadRulesReturnsParsedRuleSet(t *testing.T) {
+	ruleSet, err := readRules("./files/example.txt")
+
+	expected := rules(map[color][]color{
+		"light red":    {"bright white", "muted yellow"},
+		"dark orange":  {"bright white", "muted yellow"},
+		"bright white": {"shiny gold"},
+		"muted yellow": {"shiny gold", "faded blue"},
+		"shiny gold":   {"dark olive", "vibrant plum"},
+		"dark olive":   {"faded blue", "dotted black"},
+		"vibrant plum": {"faded blue", "dotted black"},
+		"faded blue":   {},
+		"dotted black": {},
+	})
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, ruleSet)
+}
+
+func TestTransposeRulesReturnsContainable(t *testing.T) {
+	rulesSet := rules(map[color][]color{
+		"light red":    {"bright white", "muted yellow"},
+		"dark orange":  {"bright white", "muted yellow"},
+		"bright white": {"shiny gold"},
+		"muted yellow": {"shiny gold", "faded blue"},
+		"shiny gold":   {"dark olive", "vibrant plum"},
+		"dark olive":   {"faded blue", "dotted black"},
+		"vibrant plum": {"faded blue", "dotted black"},
+		"faded blue":   {},
+		"dotted black": {},
+	})
+
+	containableMap := transposeRules(&rulesSet)
+
+	assert.Len(t, containableMap, 7)
+	assert.ElementsMatch(t, containableMap[color("bright white")], []color{"light red", "dark orange"})
+	assert.ElementsMatch(t, containableMap[color("muted yellow")], []color{"light red", "dark orange"})
+	assert.ElementsMatch(t, containableMap[color("shiny gold")], []color{"bright white", "muted yellow"})
+	assert.ElementsMatch(t, containableMap[color("faded blue")], []color{"muted yellow", "dark olive", "vibrant plum"})
+	assert.ElementsMatch(t, containableMap[color("dark olive")], []color{"shiny gold"})
+	assert.ElementsMatch(t, containableMap[color("vibrant plum")], []color{"shiny gold"})
+	assert.ElementsMatch(t, containableMap[color("dotted black")], []color{"dark olive", "vibrant plum"})
+}
+
+func TestGetTransitiveContainableForColorReturnsEmptySliceWhenNotFound(t *testing.T) {
+	containableMap := containable(map[color][]color{})
+
+	colors := getTransitiveContainableForColor(&containableMap, "green")
+
+	assert.Empty(t, colors)
+}
+
+func TestGetTransitiveContainableForColorReturnsTransitiveColorList(t *testing.T) {
+	containableMap := containable(map[color][]color{
+		"bright white": {"light red", "dark orange"},
+		"muted yellow": {"light red", "dark orange"},
+		"shiny gold":   {"bright white", "muted yellow"},
+		"faded blue":   {"muted yellow", "dark olive", "vibrant plum"},
+		"dark olive":   {"shiny gold"},
+		"vibrant plum": {"shiny gold"},
+		"dotted black": {"dark olive", "vibrant plum"},
+	})
+
+	colors := getTransitiveContainableForColor(&containableMap, "shiny gold")
+
+	assert.Equal(t, colors, map[color]bool{
+		"bright white": true,
+		"muted yellow": true,
+		"light red":    true,
+		"dark orange":  true,
+	})
 }
