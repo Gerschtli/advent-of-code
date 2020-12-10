02c315fbaa362eee4d38a369a1599fa2b27e05b1
Tobias Happ <tobias.happ@gmx.de>
2020-12-10 22:46:07 +0100

    2020/10: complete part 1

--- 2020/10/src/main.rs
+++ 2020/10/src/main.rs
@@ -7,9 +7,26 @@ use std::result;
 
 use error::Result;
 
+use crate::chain::find_chain;
+
 mod chain;
 
-fn main() {}
+fn main() -> Result<()> {
+    let chain_difference = run()?;
+
+    println!("chain difference: {}", chain_difference);
+
+    Ok(())
+}
+
+fn run() -> Result<i32> {
+    let lines = file::read_lines("./files/adapters.txt")?;
+    let adapters = parse_lines(&lines)?;
+
+    let chain_difference = find_chain(adapters);
+
+    Ok(chain_difference)
+}
 
 fn parse_lines(lines: &[String]) -> Result<Vec<i32>> {
     let numbers = lines
@@ -27,6 +44,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn run_returns_difference() {
+        let result = run();
+
+        assert_that!(&result, ok());
+        assert_that!(result.unwrap(), equal_to(2048));
+    }
+
+    #[test]
     fn parse_lines_returns_list_of_ints() {
         let numbers = parse_lines(&vec![
             "35".to_string(),
@@ -40,7 +65,7 @@ mod tests {
         assert_that!(
             &numbers.unwrap(),
             contains(vec![35, 20, 15, 25, 47]).exactly().in_order()
-        )
+        );
     }
 
     #[test]
@@ -57,6 +82,6 @@ mod tests {
         assert_that!(
             format!("{}", numbers.unwrap_err()),
             equal_to("app error: invalid digit found in string")
-        )
+        );
     }
 }
