015213e0a0a46b2630b51c27fd1672bf47cb7f17
Tobias Happ <tobias.happ@gmx.de>
2020-12-10 21:00:50 +0100

    2020/09: add findWeakness method

--- 2020/09/output.go
+++ 2020/09/output.go
@@ -26,3 +26,33 @@ func (o *output) isValid(index int) bool {
 
 	return false
 }
+
+func (o *output) findWeakness(invalidNumber int) (int, bool) {
+	length := len(o.numbers)
+
+	for i := 0; i < length-1; i++ {
+		firstValue := o.numbers[i]
+		smallest, largest, sum := firstValue, firstValue, firstValue
+
+		for j := i + 1; j < length; j++ {
+			lastValue := o.numbers[j]
+			sum += lastValue
+
+			if lastValue < smallest {
+				smallest = lastValue
+			}
+			if lastValue > largest {
+				largest = lastValue
+			}
+
+			if sum == invalidNumber {
+				return smallest + largest, true
+			}
+			if sum > invalidNumber {
+				break
+			}
+		}
+	}
+
+	return 0, false
+}
--- 2020/09/output_test.go
+++ 2020/09/output_test.go
@@ -59,3 +59,57 @@ func TestOutputFindFirstInvalidReturnsFalseIfEveryNumberIsValid(t *testing.T) {
 
 	assert.Equal(t, false, found)
 }
+
+func TestOutputFindWeaknessReturnsWeaknessNumber(t *testing.T) {
+	o := output{
+		[]int{
+			35,
+			20,
+			15,
+			25,
+			47,
+			40,
+			62,
+			55,
+			65,
+			95,
+			102,
+			117,
+			150,
+			182,
+			127,
+			219,
+			299,
+			277,
+			309,
+			576,
+		},
+		5,
+	}
+
+	weakness, found := o.findWeakness(127)
+
+	assert.Equal(t, true, found)
+	assert.Equal(t, 62, weakness)
+}
+
+func TestOutputFindWeaknessReturnsFalseWhenNoWeaknessFound(t *testing.T) {
+	o := output{
+		[]int{
+			1,
+			2,
+			3,
+			4,
+			6,
+			11,
+			9,
+			10,
+			19,
+		},
+		4,
+	}
+
+	_, found := o.findWeakness(11)
+
+	assert.Equal(t, false, found)
+}
