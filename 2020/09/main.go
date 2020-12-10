ea905aa848239e543126cecef101e1dbbeb9004b
Tobias Happ <tobias.happ@gmx.de>
2020-12-10 20:21:09 +0100

    2020/09: add parseFile function

--- 2020/09/main.go
+++ 2020/09/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"strconv"
+
+	"github.com/Gerschtli/advent-of-code/lib/go/file"
+)
+
 func main() {
+}
+
+func parseFile(filename string) ([]int, error) {
+	var numbers []int
+	err := file.ReadFile(filename, func(index int, line string) error {
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			return err
+		}
+
+		numbers = append(numbers, num)
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return numbers, nil
 }
--- 2020/09/main_test.go
+++ 2020/09/main_test.go
@@ -21,3 +21,18 @@ func TestMainLogsResults(t *testing.T) {
 	assert.Len(t, lines, 1)
 	assert.Empty(t, lines[0])
 }
+
+func TestParseFileReturnsSliceOfInts(t *testing.T) {
+	numbers, err := parseFile("./files/example.txt")
+
+	assert.Nil(t, err)
+	assert.Equal(t, []int{35, 20, 15, 25, 47}, numbers)
+}
+
+func TestParseFileReturnsErrorForInvalidLink(t *testing.T) {
+	_, err := parseFile("./files/example_invalid.txt")
+
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "strconv.Atoi: parsing \"abc\": invalid syntax", err.Error())
+	}
+}
