b808adc6adcff4d30da17416ab3f6290bad11eaf
Tobias Happ <tobias.happ@gmx.de>
2020-12-12 15:05:19 +0100

    2020/11: complete part 1

--- 2020/11/main.go
+++ 2020/11/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"log"
+	"reflect"
+
 	"github.com/Gerschtli/advent-of-code/lib/go/file"
 )
 
 func main() {
+	seatsMap, err := parseSeats("./files/seats.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentSeats, newSeats := seatsMap, seats{}
+	for {
+		newSeats = currentSeats.runRound()
+
+		if reflect.DeepEqual(currentSeats, newSeats) {
+			log.Printf("%d occupied seats\n", newSeats.countAllOccupied())
+			break
+		}
+
+		currentSeats = newSeats
+	}
 }
 
 func parseSeats(filename string) (seats, error) {
