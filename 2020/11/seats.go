d76af53ac8dcddddcb35417cd36b6c175fdfe243
Tobias Happ <tobias.happ@gmx.de>
2020-12-12 14:56:08 +0100

    2020/11:: add seat.runRound method

--- 2020/11/seats.go
+++ 2020/11/seats.go
@@ -9,3 +9,64 @@ const (
 )
 
 type seats [][]status
+
+func (s *seats) runRound() seats {
+	var newSeats seats
+
+	for i := range *s {
+		var row []status
+		for j, value := range (*s)[i] {
+			newValue := value
+			count := s.countOccupied(i, j)
+
+			if value == statusFree && count == 0 {
+				newValue = statusOccupied
+			}
+			if value == statusOccupied && count >= 4 {
+				newValue = statusFree
+			}
+
+			row = append(row, newValue)
+		}
+
+		newSeats = append(newSeats, row)
+	}
+
+	return newSeats
+}
+
+func (s *seats) countOccupied(y int, x int) int {
+	var count int
+
+	for i := max(y-1, 0); i <= min(y+1, len(*s)-1); i++ {
+		for j := max(x-1, 0); j <= min(x+1, len((*s)[i])-1); j++ {
+			if i == y && j == x {
+				continue
+			}
+
+			value := (*s)[i][j]
+
+			if value == statusOccupied {
+				count += 1
+			}
+		}
+	}
+
+	return count
+}
+
+func min(i, i2 int) int {
+	if i <= i2 {
+		return i
+	}
+
+	return i2
+}
+
+func max(i, i2 int) int {
+	if i >= i2 {
+		return i
+	}
+
+	return i2
+}
---
+++ 2020/11/seats_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSeatsRunRoundReplacesFreeWithOccupiedWithFirstInitialData(t *testing.T) {
+	seatsInitial := seats([][]status{
+		{statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusFree, statusFloor, statusFree, statusFloor, statusFree, statusFloor, statusFloor, statusFree, statusFloor, statusFloor},
+		{statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusFree, statusFloor, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusFloor, statusFloor, statusFree, statusFloor, statusFree, statusFloor, statusFloor, statusFloor, statusFloor, statusFloor},
+		{statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree},
+		{statusFree, statusFloor, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree},
+		{statusFree, statusFloor, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree, statusFree},
+	})
+
+	seatsRound1 := seatsInitial.runRound()
+
+	expected := seats([][]status{
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusFloor, statusOccupied, statusFloor, statusFloor, statusOccupied, statusFloor, statusFloor},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusFloor, statusFloor, statusOccupied, statusFloor, statusOccupied, statusFloor, statusFloor, statusFloor, statusFloor, statusFloor},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+	})
+
+	assert.Equal(t, expected, seatsRound1)
+}
+
+func TestSeatsRunRoundFollowsRules(t *testing.T) {
+	seatsRound1 := seats([][]status{
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusFloor, statusOccupied, statusFloor, statusFloor, statusOccupied, statusFloor, statusFloor},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusFloor, statusFloor, statusOccupied, statusFloor, statusOccupied, statusFloor, statusFloor, statusFloor, statusFloor, statusFloor},
+		{statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied},
+		{statusOccupied, statusFloor, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+	})
+
+	seatsRound2 := seatsRound1.runRound()
+
+	expected := seats([][]status{
+		{statusOccupied, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusOccupied, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree, statusOccupied},
+		{statusFree, statusFloor, statusFree, statusFloor, statusFree, statusFloor, statusFloor, statusFree, statusFloor, statusFloor},
+		{statusOccupied, statusFree, statusFree, statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusOccupied},
+		{statusOccupied, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree, statusFloor, statusFree, statusFree},
+		{statusOccupied, statusFloor, statusFree, statusFree, statusFree, statusFree, statusOccupied, statusFloor, statusOccupied, statusOccupied},
+		{statusFloor, statusFloor, statusFree, statusFloor, statusFree, statusFloor, statusFloor, statusFloor, statusFloor, statusFloor},
+		{statusOccupied, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusOccupied},
+		{statusOccupied, statusFloor, statusFree, statusFree, statusFree, statusFree, statusFree, statusFree, statusFloor, statusFree},
+		{statusOccupied, statusFloor, statusOccupied, statusFree, statusFree, statusFree, statusFree, statusFloor, statusOccupied, statusOccupied},
+	})
+
+	assert.Equal(t, expected, seatsRound2)
+}
