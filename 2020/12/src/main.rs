c0657fa4c9e92b4e5db6a23ab2511055e7502b96
Tobias Happ <tobias.happ@gmx.de>
2020-12-17 14:57:09 +0100

    2020/12: print fixed distance

--- 2020/12/src/main.rs
+++ 2020/12/src/main.rs
@@ -7,29 +7,36 @@ use error::Result;
 
 use crate::instruction::Instructions;
 use crate::state::State;
+use crate::state_fixed::StateFixed;
 
 mod instruction;
 mod state;
 mod state_fixed;
 
 fn main() -> Result<()> {
-    let distance = get_distance()?;
+    let (distance, distance_fixed) = get_distance()?;
 
     println!("manhattan distance: {}", distance);
+    println!("manhattan distance (fixed): {}", distance_fixed);
 
     Ok(())
 }
 
-fn get_distance() -> Result<i32> {
+fn get_distance() -> Result<(i32, i32)> {
     let lines = file::read_lines("./files/instructions.txt")?;
     let instructions = parse_instructions(&lines)?;
     let mut state = State::new();
+    let mut state_fixed = StateFixed::new();
 
     for instruction in instructions.get_all() {
         state.apply(instruction);
+        state_fixed.apply(instruction);
     }
 
-    Ok(state.get_manhattan_distance())
+    Ok((
+        state.get_manhattan_distance(),
+        state_fixed.get_manhattan_distance(),
+    ))
 }
 
 fn parse_instructions(lines: &[String]) -> Result<Instructions> {
@@ -52,7 +59,7 @@ mod tests {
 
     #[test]
     fn get_distance_returns_result() {
-        assert_that!(get_distance(), has(962));
+        assert_that!(get_distance(), has((962, 56135)));
     }
 
     #[test]
