358658e8a73710f6fc8e9b4c54b6ce4ada8c305f
Tobias Happ <tobias.happ@gmx.de>
2020-12-17 13:42:24 +0100

    2020/12: add parse_instructions

--- 2020/12/src/main.rs
+++ 2020/12/src/main.rs
@@ -3,13 +3,56 @@
 #[macro_use]
 extern crate hamcrest2;
 
+use error::Result;
+
+use crate::instruction::Instructions;
+
 mod instruction;
 
 fn main() {}
+
+fn parse_instructions(lines: &[String]) -> Result<Instructions> {
+    let mut instructions = Instructions::new();
+
+    for line in lines {
+        instructions.add(line)?;
+    }
+
+    Ok(instructions)
+}
 
 #[cfg(test)]
 mod tests {
     use hamcrest2::prelude::*;
 
+    use crate::instruction::Instruction;
+
     use super::*;
+
+    #[test]
+    fn parse_instructions_returns_parsed_instructions() {
+        let lines = vec!["N12".to_string(), "W7".to_string()];
+        let result = parse_instructions(&lines);
+
+        assert_that!(&result, ok());
+        assert_that!(
+            result,
+            has(Instructions::init(vec![
+                Instruction::North(12),
+                Instruction::West(7),
+            ]))
+        );
+    }
+
+    #[test]
+    fn parse_instructions_returns_error_for_invalid_lines() {
+        let lines = vec!["N12".to_string(), "A7".to_string()];
+        let result = parse_instructions(&lines);
+
+        assert_that!(&result, err());
+        assert_that!(
+            format!("{}", result.unwrap_err()),
+            equal_to("app error: unknown instruction: A7")
+        );
+    }
 }
