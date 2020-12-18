d5cf866a7d5906338148cf2e0409251e16145317
Tobias Happ <tobias.happ@gmx.de>
2020-12-18 22:46:51 +0100

    2020/15: implement part 1

--- 2020/15/main.go
+++ 2020/15/main.go
@@ -1,4 +1,34 @@
 package main
 
+import "log"
+
 func main() {
+	number := getNumber([]int{16, 12, 1, 0, 15, 7, 11}, 2020)
+	log.Printf("2020th number: %d\n", number)
+}
+
+func getNumber(starting []int, limit int) int {
+	numbers := starting
+
+	for i := len(starting) - 1; i < limit; i++ {
+		lastNumber := numbers[i]
+		lastOccurrence, found := getLastOccurrence(lastNumber, numbers[:i])
+		if found {
+			numbers = append(numbers, i-lastOccurrence)
+		} else {
+			numbers = append(numbers, 0)
+		}
+	}
+
+	return numbers[limit-1]
+}
+
+func getLastOccurrence(needle int, haystack []int) (int, bool) {
+	for i := len(haystack) - 1; i >= 0; i-- {
+		if haystack[i] == needle {
+			return i, true
+		}
+	}
+
+	return 0, false
 }
--- 2020/15/main_test.go
+++ 2020/15/main_test.go
@@ -18,6 +18,17 @@ func TestMainLogsResults(t *testing.T) {
 
 	lines := strings.Split(buf.String(), "\n")
 
-	assert.Len(t, lines, 1)
-	assert.Empty(t, lines[0])
+	assert.Len(t, lines, 2)
+	assert.Contains(t, lines[0], "2020th number: 403")
+	assert.Empty(t, lines[1])
+}
+
+func TestGetNumber(t *testing.T) {
+	assert.Equal(t, 0, getNumber([]int{0, 3, 6}, 10))
+	assert.Equal(t, 1, getNumber([]int{1, 3, 2}, 2020))
+	assert.Equal(t, 10, getNumber([]int{2, 1, 3}, 2020))
+	assert.Equal(t, 27, getNumber([]int{1, 2, 3}, 2020))
+	assert.Equal(t, 78, getNumber([]int{2, 3, 1}, 2020))
+	assert.Equal(t, 438, getNumber([]int{3, 2, 1}, 2020))
+	assert.Equal(t, 1836, getNumber([]int{3, 1, 2}, 2020))
 }
