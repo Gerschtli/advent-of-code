8e6cfcb2ac8e3fd37f390b87e9807bbb57d8cc42
Tobias Happ <tobias.happ@gmx.de>
2020-12-19 13:36:59 +0100

    2020/16: add Ticket::is_valid_for_any_rule

--- 2020/16/src/data.rs
+++ 2020/16/src/data.rs
@@ -15,6 +15,16 @@ impl Rule {
             ranges: ranges.into(),
         }
     }
+
+    fn is_valid_value(&self, value: i32) -> bool {
+        for (low, high) in &self.ranges {
+            if *low <= value && value <= *high {
+                return true;
+            }
+        }
+
+        false
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -30,5 +40,56 @@ impl Ticket {
         Self {
             values: values.into(),
         }
+    }
+
+    pub(super) fn is_valid_for_any_rule(&self, rules: &[Rule]) -> (bool, i32) {
+        let result = self
+            .values
+            .iter()
+            .filter(|value| {
+                let count_matching_rules = rules
+                    .iter()
+                    .filter(|rule| rule.is_valid_value(**value))
+                    .count();
+
+                count_matching_rules == 0
+            })
+            .next();
+
+        match result {
+            Some(value) => (false, *value),
+            None => (true, 0),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use hamcrest2::prelude::*;
+
+    use super::*;
+
+    #[test]
+    fn ticket_is_valid_for_any_rule_returns_true_for_valid() {
+        let ticket = Ticket::new(vec![7, 3, 47]);
+        let rules = vec![
+            Rule::new("class", vec![(1, 3), (5, 7)]),
+            Rule::new("row", vec![(6, 11), (33, 44)]),
+            Rule::new("seat", vec![(13, 40), (45, 50)]),
+        ];
+
+        assert_that!(ticket.is_valid_for_any_rule(&rules), equal_to((true, 0)));
+    }
+
+    #[test]
+    fn ticket_is_valid_for_any_rule_returns_false_and_error_value_for_invalid() {
+        let ticket = Ticket::new(vec![40, 4, 50]);
+        let rules = vec![
+            Rule::new("class", vec![(1, 3), (5, 7)]),
+            Rule::new("row", vec![(6, 11), (33, 44)]),
+            Rule::new("seat", vec![(13, 40), (45, 50)]),
+        ];
+
+        assert_that!(ticket.is_valid_for_any_rule(&rules), equal_to((false, 4)));
     }
 }
