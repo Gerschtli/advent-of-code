3a00d7c002bfb2d10ba679e799f48dd821743dfc
Tobias Happ <tobias.happ@gmx.de>
2021-12-01 22:41:17 +0100

    2021/01: implement part 2

--- 2021/01/code.nix
+++ 2021/01/code.nix
@@ -1,13 +1,18 @@
 { pkgs }:
 
 let
-  countIncreases = file:
+  readFileToInts = file:
     let
-      inherit (builtins) head tail;
-      inherit (pkgs.lib) fileContents foldl splitString toInt;
+      inherit (pkgs.lib) fileContents splitString toInt;
 
       lines = splitString "\n" (fileContents file);
-      numbers = map toInt lines;
+    in
+    map toInt lines;
+
+  countIncreases = numbers:
+    let
+      inherit (builtins) head tail;
+      inherit (pkgs.lib) foldl;
 
       increaseCounter = foldl
         ({ last, counter }: num: {
@@ -21,24 +26,52 @@ let
         (tail numbers);
     in
     increaseCounter.counter;
+
+  countIncreasesOfFile = file: countIncreases (readFileToInts file);
+
+  countSlidingWindowIncreases = file:
+    let
+      inherit (builtins) elemAt genList length;
+
+      numbers = readFileToInts file;
+      getNumber = elemAt numbers;
+
+      windowSums = genList
+        (n: (getNumber n) + (getNumber (n + 1)) + (getNumber (n + 2)))
+        (length numbers - 2);
+    in
+    countIncreases windowSums;
 in
 
 {
   tests = [
     {
       description = "count of increments of example input is 7";
-      actual = countIncreases ./input-example.txt;
+      actual = countIncreasesOfFile ./input-example.txt;
       expected = 7;
     }
 
     {
       description = "count of increments of input.txt is 1292";
-      actual = countIncreases ./input.txt;
+      actual = countIncreasesOfFile ./input.txt;
       expected = 1292;
+    }
+
+    {
+      description = "count of sliding window increments of example input is 5";
+      actual = countSlidingWindowIncreases ./input-example.txt;
+      expected = 5;
+    }
+
+    {
+      description = "count of sliding window increments of input.txt is 1262";
+      actual = countSlidingWindowIncreases ./input.txt;
+      expected = 1262;
     }
   ];
 
   solution = ''
-    Count of larger measurements: ${toString (countIncreases ./input.txt)}
+    Count of larger measurements: ${toString (countIncreasesOfFile ./input.txt)}
+    Count of larger measurements within sliding window: ${toString (countSlidingWindowIncreases ./input.txt)}
   '';
 }
