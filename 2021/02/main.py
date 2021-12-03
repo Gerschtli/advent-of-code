468ade059974c9bd8a37af0e7e2dbd06916dd8f2
Tobias Happ <tobias.happ@gmx.de>
2021-12-03 19:52:13 +0100

    2021/02: implement part 2

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
+from typing import Tuple
 
 
 class Direction(Enum):
@@ -35,6 +36,28 @@ class Position:
         return Position(x, y)
 
 
+@dataclass(frozen=True)
+class PositionWithAim:
+    aim: int = 0
+    x: int = 0
+    y: int = 0
+
+    def move(self, command: Command) -> PositionWithAim:
+        aim = self.aim
+        x = self.x
+        y = self.y
+
+        if command.direction == Direction.DOWN:
+            aim += command.size
+        elif command.direction == Direction.UP:
+            aim -= command.size
+        elif command.direction == Direction.FORWARD:
+            x += command.size
+            y += command.size * aim
+
+        return PositionWithAim(aim, x, y)
+
+
 def read_file(path: str) -> [str]:
     with open(path, 'r') as file:
         return [line.strip() for line in file.readlines()]
@@ -43,7 +66,7 @@ def read_file(path: str) -> [str]:
 def parse_commands(lines: [str]) -> [Command]:
     commands = []
     for line in lines:
-        [direction, size] = line.split(" ")
+        direction, size = line.split(" ")
 
         commands.append(
             Command(
@@ -55,21 +78,24 @@ def parse_commands(lines: [str]) -> [Command]:
     return commands
 
 
-def evaluate_position(path: str) -> Position:
+def evaluate_positions(path: str) -> Tuple[Position, PositionWithAim]:
     lines = read_file(path)
     commands = parse_commands(lines)
 
     position = Position()
+    position_with_aim = PositionWithAim()
     for command in commands:
         position = position.move(command)
+        position_with_aim = position_with_aim.move(command)
 
-    return position
+    return position, position_with_aim
 
 
 def main() -> None:
-    position = evaluate_position('./files/input.txt')
+    position, position_with_aim = evaluate_positions('./files/input.txt')
 
     print(f'multiplication of x and y: {position.x * position.y}')
+    print(f'multiplication of x and y with aim: {position_with_aim.x * position_with_aim.y}')
 
 
 if __name__ == '__main__':
--- 2021/02/main_test.py
+++ 2021/02/main_test.py
@@ -1,4 +1,4 @@
-from main import Command, Direction, Position, evaluate_position, parse_commands, read_file
+from main import Command, Direction, Position, PositionWithAim, evaluate_positions, parse_commands, read_file
 
 
 def test_read_file_returns_lines_of_file_as_list() -> None:
@@ -55,7 +55,7 @@ def test_position_move_increases_y_value_given_a_down_command() -> None:
     assert new_position.y == 2
 
 
-def test_position_move_decreases_y_value_given_a_up_command() -> None:
+def test_position_move_decreases_y_value_given_an_up_command() -> None:
     position = Position()
 
     new_position = position.move(Command(Direction.UP, 2))
@@ -64,15 +64,59 @@ def test_position_move_decreases_y_value_given_a_up_command() -> None:
     assert new_position.y == -2
 
 
-def test_evaluate_position_returns_x_15_y_10_for_example() -> None:
-    position = evaluate_position('./files/input-example.txt')
+def test_position_with_aim_inits_with_aim_x_and_y_0() -> None:
+    position = PositionWithAim()
+
+    assert position.aim == 0
+    assert position.x == 0
+    assert position.y == 0
+
+
+def test_position_with_aim_increases_aim_given_a_down_command() -> None:
+    position = PositionWithAim()
+
+    new_position = position.move(Command(Direction.DOWN, 3))
+
+    assert new_position.aim == 3
+    assert new_position.x == 0
+    assert new_position.y == 0
+
+
+def test_position_with_aim_decreases_aim_given_an_up_command() -> None:
+    position = PositionWithAim()
+
+    new_position = position.move(Command(Direction.UP, 2))
+
+    assert new_position.aim == -2
+    assert new_position.x == 0
+    assert new_position.y == 0
+
+
+def test_position_with_aim_changes_x_and_y_given_an_forward_command() -> None:
+    position = PositionWithAim(aim=5)
+
+    new_position = position.move(Command(Direction.FORWARD, 3))
+
+    assert new_position.aim == 5
+    assert new_position.x == 3
+    assert new_position.y == 15
+
+
+def test_evaluate_positions_for_example() -> None:
+    position, position_with_aim = evaluate_positions('./files/input-example.txt')
 
     assert position.x == 15
     assert position.y == 10
 
+    assert position_with_aim.x == 15
+    assert position_with_aim.y == 60
 
-def test_evaluate_position_returns_x_1968_y_1063_for_input() -> None:
-    position = evaluate_position('./files/input.txt')
+
+def test_evaluate_positions_for_input() -> None:
+    position, position_with_aim = evaluate_positions('./files/input.txt')
 
     assert position.x == 1968
     assert position.y == 1063
+
+    assert position_with_aim.x == 1968
+    assert position_with_aim.y == 1060092
